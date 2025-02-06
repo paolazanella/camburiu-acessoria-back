98ac915fae73e20929118c191dafd3dea3f66d8b
Paola Zanella <pzanella@edu.univali.br>
2025-02-06 10:09:18 -0300

    primeiro commit

---
+++ src/main/java/br/com/camburiu/camburiu_acessoria/CamburiuAcessoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.camburiu.camburiu_acessoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamburiuAcessoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamburiuAcessoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/camburiu/camburiu_acessoria/CamburiuAcessoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.camburiu.camburiu_acessoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamburiuAcessoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
