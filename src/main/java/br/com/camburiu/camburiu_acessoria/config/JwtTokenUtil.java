73e463ed5d811e1734a989d70c020010bc237fd8
Paola Zanella <pzanella@edu.univali.br>
2025-02-06 11:05:59 -0300

    criando arquivos para configuracao do serverse

---
+++ src/main/java/br/com/camburiu/camburiu_acessoria/config/JwtTokenUtil.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package br.com.camburiu.camburiu_acessoria.config;
+
+/**
+ *
+ * @author Paola Zanella
+ */
+public class JwtTokenUtil {
+
+}
