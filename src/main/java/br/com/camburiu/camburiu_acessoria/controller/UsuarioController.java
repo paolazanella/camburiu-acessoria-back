3ca396ce904a4109925bbe83f2177673ba1a0cc9
Paola Zanella <pzanella@edu.univali.br>
2025-02-07 15:57:08 -0300

    falata, atenticacao melhorar o controller para parar de dar erro 401(colocar autenciticao)

--- src/main/java/br/com/camburiu/camburiu_acessoria/controller/UsuarioController.java
+++ src/main/java/br/com/camburiu/camburiu_acessoria/controller/UsuarioController.java
@@ -2,21 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package br.com.camburiu.camburiu_acessoria.controller;
-
-import br.com.camburiu.camburiu_acessoria.model.Usuario;
-import br.com.camburiu.camburiu_acessoria.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.camburiu.camburiu_acessoria.model.Usuario;
+import br.com.camburiu.camburiu_acessoria.repository.UsuarioRepository;
+
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
+
     @Autowired
     private UsuarioRepository usuarioRepository;
 
